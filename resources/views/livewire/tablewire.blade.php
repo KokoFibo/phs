35dc324f24a4425ee04d62d9c08b5b931d1707bd
Anton <kokonacci@gmail.com>
2023-02-18 11:10:53 +0700

    penyempurnaan 11:10

--- resources/views/livewire/tablewire.blade.php
+++ resources/views/livewire/tablewire.blade.php
@@ -1,14 +1,12 @@
 <div>
 
-
-
       {{-- Search Bar --}}
       <div class="items-center w-full md:flex">
             <div class="w-3/4 mx-5 ">
                   <div class="items-center md:flex">
                         {{-- Search --}}
                         <div class="w-full mt-3 mr-3 md:w-1/5">
-                              <input type="text" class="w-full px-4 py-1 text-purple-700 border border-purple-700 rounded " wire:model="search" placeholder="{{ __('Search') }}">
+                              <input type="search" class="w-full px-4 py-1 text-purple-700 border border-purple-700 rounded " wire:model="search" placeholder="&#128270 {{ __('Search') }}">
                         </div>
                         {{-- Category --}}
                         <div class="w-full mt-3 mr-3 md:w-1/5">
