d59af2cb1c59965f4896c62cee6a80552fda847c
Anton <kokonacci@gmail.com>
2023-03-01 11:16:11 +0700

    fixed bug dashboard umat active dan inactive

--- resources/views/livewire/testaja.blade.php
+++ resources/views/livewire/testaja.blade.php
@@ -1,3 +1,27 @@
 <div>
-      hello rows = {{$usersetting[0]['status'] }}
+      <div>
+            <label>pilih group :</label>
+            <select wire:model="selectedGroup">
+                  <option value="">Pilih group</option>
+                  @foreach ($group as $g)
+                  <option value="{{ $g->id }}">{{ $g->nama_group }}</option>
+                  @endforeach
+            </select>
+      </div>
+
+      <div>
+            <label>pilih Vihara :</label>
+            <select wire:model="selectedBranch">
+                  <option value="">Pilih Vihara</option>
+                  @foreach ($vihara as $v)
+                  <option value="{{ $v->id }}">{{ $v->nama_branch }}</option>
+                  @endforeach
+            </select>
+      </div>
+      <p>Seleceted Group : {{ $selectedGroup }}</p>
+      <p>Seleceted Branch : {{ $selectedBranch }}</p>
+      <p>Data Umat Aktif di {{ $selectedGroup }} {{ $selectedBranch }} = {{ $dataUmatAktif }} Orang.</p>
+      <p>Data Umat Tidak Aktif di {{ $selectedGroup }} {{ $selectedBranch }} = {{ $dataUmatTidakAktif }} Orang.</p>
+      <p>Total Data Umat di {{ $selectedGroup }} {{ $selectedBranch }} = {{ $totalUmat }} Orang.</p>
+      <p>Umat YTD {{ $selectedGroup }} {{ $selectedBranch }} = {{ $umatYTD }} Orang.</p>
 </div>
