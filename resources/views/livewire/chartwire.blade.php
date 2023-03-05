d5891cb36010431503473e17d50801402da30ae6
Anton <kokonacci@gmail.com>
2023-03-05 18:49:05 +0700

    sampai sini masih ok

--- resources/views/livewire/chartwire.blade.php
+++ resources/views/livewire/chartwire.blade.php
@@ -36,6 +36,9 @@
     <div style="width:400px">
         <canvas id="myChart"></canvas>
     </div>
+    <div style="width:400px">
+        <canvas id="myChart1"></canvas>
+    </div>
 
     @push('script')
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
@@ -88,6 +91,60 @@
                         dataset.data = (peserta);
                   });
                   myChart.update();
+            });
+
+    </script>
+    {{-- pie Chart --}}
+    <script>
+        var tanggal = JSON.parse(`<?php echo $dataXjson; ?>`);
+        var peserta = JSON.parse(`<?php echo $dataYjson; ?>`);
+        const ctx1 = document.getElementById('myChart1').getContext('2d');
+            const myChart1 = new Chart(ctx1, {
+                  type: 'pie'
+                  , data: {
+                        // labels: tanggal
+                        labels: (['Vegetarian Total','Sidang Dharma 3 Hari', 'Belum Keduanya'])
+                        , datasets: [{
+                              label: '# of Votes'
+                              , data: peserta
+                              , backgroundColor: [
+                                    'rgba(255, 99, 132, 0.2)'
+                                    , 'rgba(54, 162, 235, 0.2)'
+                                    , 'rgba(255, 206, 86, 0.2)'
+                                    , 'rgba(75, 192, 192, 0.2)'
+                                    , 'rgba(153, 102, 255, 0.2)'
+                                    , 'rgba(255, 159, 64, 0.2)'
+                              ]
+                              , borderColor: [
+                                    'rgba(255, 99, 132, 1)'
+                                    , 'rgba(54, 162, 235, 1)'
+                                    , 'rgba(255, 206, 86, 1)'
+                                    , 'rgba(75, 192, 192, 1)'
+                                    , 'rgba(153, 102, 255, 1)'
+                                    , 'rgba(255, 159, 64, 1)'
+                              ]
+                              , borderWidth: 1
+                        }]
+                  }
+                  , options: {
+                        scales: {
+                              y: {
+                                    beginAtZero: true
+                              }
+                        }
+                  }
+            });
+
+
+            Livewire.on('berhasilUpdate', event => {
+                var tanggal = JSON.parse(event.dataXjson);
+                var peserta = JSON.parse(event.dataYjson);
+                console.log(tanggal, peserta);
+                  myChart1.data.labels = (['Vegetarian Total','Sidang Dharma 3 Hari', 'Belum Keduanya']);
+                  myChart1.data.datasets.forEach((dataset) => {
+                        dataset.data = (peserta);
+                  });
+                  myChart1.update();
             });
 
     </script>
