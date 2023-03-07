26d0ef2a397f0b7fadd32d39f2165548ffa6fc0a
Anton <kokonacci@gmail.com>
2023-03-07 15:54:49 +0700

    ok

---
+++ app/Http/Livewire/Sementara.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Sementara extends Component
+{
+    public function render()
+    {
+        return view('livewire.sementara');
+    }
+}
