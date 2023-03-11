7d32e16d924facddedfe215de8b7e9b720aaae9c
Anton <kokonacci@gmail.com>
2023-03-11 09:27:37 +0700

    rubah registration agar ux lebih baik lagi

--- resources/views/livewire/panditawire.blade copy.php
+++
@@ -1,9 +0,0 @@
-<div>
-    {{-- @include('datapelita.addPanditaModal') --}}
-
-    <button type="button" class="btn  btn-primary" data-toggle="modal" data-target="#AddPanditaModal">
-        {{-- <button type="button" class="btn  btn-primary" data-toggle="modal" data-target="#AddPanditaModal"
-        wire:click="clearSession"> --}}
-        <i class="fa fa-plus-circle"></i> {{ __('Add Data Pandita') }}
-    </button>
-</div>
